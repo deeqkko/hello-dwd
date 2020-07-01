5f83501ea0dbdb0e406e075066c268539c58aca3
deeqkko <kimmo.koivusalo@gmail.com>
2020-07-01 20:25:31 +0300

    index.js new message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello DevOps with Docker!')
+  response.send(`Hello DevOps with Docker! Now it's working!`)
 })
 
 app.listen(app.get('port'), function() {
