7e326b00c4e8a781699db96dfbc663e7bfc43441
deeqkko <kimmo.koivusalo@gmail.com>
2020-07-01 20:29:28 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(`Hello DevOps with Docker! Now it's working!`)
+  response.send('Hello DevOps with Docker! Now it is working!')
 })
 
 app.listen(app.get('port'), function() {
